cc9b5c169f7bc41098cee9ccb137c574f7d77687
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-08-26 11:07:30 -0300

    Avance del proyecto

--- peliculas.elc.lat/app.js
+++ peliculas.elc.lat/app.js
@@ -1,20 +1,19 @@
 "use strict";
-// Start-up - última carpeta git subida: 1.04
-console.clear();
+console.time("Comienzo")
 
 // Requires
-global.path = require("path");
-global.express = require("express");
+const path = require("path");
+const express = require("express");
 const app = express();
 
 // Para usar cookies
 const cookies = require("cookie-parser");
 app.use(cookies());
 
-// Variables que toman valores de 'path'
-const entProd = global.path.basename(__dirname) == "1-Actual";
-global.entPrueba = global.path.basename(__dirname) == "2-Prueba";
-global.entDesarr = !entProd && !entPrueba;
+// Entornos
+const entProd = path.basename(__dirname) == "1-Actual";
+const entPrueba = path.basename(__dirname) == "2-Prueba";
+const entDesarr = !entProd && !entPrueba;
 
 // Listener
 const puerto = entProd ? 4210 : entPrueba ? 4207 : 3001;
@@ -25,12 +24,10 @@ if (entDesarr) {
 	https.createServer(opciones, app).listen(puerto, () => console.log("\nELC Películas Redirecciona - Servidor funcionando...")); // Para conectarse con el servidor
 } else app.listen(puerto, () => console.log("\nELC Películas Redirecciona - Servidor funcionando...")); // Para conectarse con el servidor
 
-// Host
+// Redirige
 const inicio = "https://peliculas";
 const elc = "evangelicemoslacultura";
 const urlHost = entProd ? `${inicio}.${elc}.com` : entPrueba ? `${inicio}2.${elc}.com` : `${inicio}.${elc}:3006`;
-
-// Redirige
 app.use((req, res) => {
 	// Variables
 	const {cliente_id, email, clienteYaMigrado} = req.cookies;
@@ -49,5 +46,6 @@ app.use((req, res) => {
 	// Redirige a 'peliculas.evangelicemoslacultura'
 	// req.originalUrl += "&sinCookie=true"
 	// return res.send(req.cookies);
+	console.timeEnd("Comienzo");
 	return res.redirect(urlHost + req.originalUrl);
 });
