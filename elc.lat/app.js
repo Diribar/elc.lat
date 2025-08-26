36be8cf6389030267a7060123c3ce5fb5ac9df7f
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-08-26 10:44:06 -0300

    Avance del proyecto

--- elc.lat/app.js
+++ elc.lat/app.js
@@ -1,18 +1,17 @@
 "use strict";
 
 // Entorno
-global.path = require("path");
+const path = require("path");
 const carpeta = path.basename(path.join(__dirname));
 const entProd = carpeta == "1-Actual";
 const entPrueba = carpeta == "2-Prueba";
 const entDes = !entProd && !entPrueba;
 
 // Host
-global.urlHost = entProd ? "https://peliculas.elc.lat" : entPrueba ? "https://peliculas2.elc.lat" : "https://peliculas.elc:3001";
-if (!entDes) global.dominio = {domain: "elc.lat"};
+const urlHost = entProd ? "https://peliculas.elc.lat" : entPrueba ? "https://peliculas2.elc.lat" : "https://peliculas.elc:3001";
 
 // Otros requires
-global.express = require("express");
+const express = require("express");
 const app = express();
 
 // Para usar cookies
@@ -23,18 +22,8 @@ app.use(cookies());
 const puerto = entProd ? 4204 : entPrueba ? 4201 : 80;
 app.listen(puerto, () => console.log("\nELC Redirecciona - Servidor funcionando..."));
 
-// Redirige
-const unaHora = 60 * 60 * 1000;
-const unDia = unaHora * 24;
-const unAno = unDia * 365;
-
+// Redirige a 'películas'
 app.use((req, res) => {
-	// Actualiza las cookies para que sean compartidas
-	if (req.cookies && !entDes) {
-		if (req.cookies.email) res.cookie("email", req.cookies.email, {maxAge: unAno, ...global.dominio});
-		if (req.cookies.cliente_id) res.cookie("cliente_id", req.cookies.cliente_id, {maxAge: unAno, ...global.dominio});
-	}
-
-	// Redirige a 'películas'
+	return res.send(req.cookies);
 	return res.redirect(urlHost + req.originalUrl);
 });
