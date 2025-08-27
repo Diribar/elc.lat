e75fd7b192ea9e85555ba50506639e8a633c9e06
Diego Iribarren <76135160+Diribar@users.noreply.github.com>
2025-08-27 20:43:55 -0300

    Avance del proyecto

--- elc.lat/app.js
+++ elc.lat/app.js
@@ -6,10 +6,6 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// Para usar cookies
-const cookies = require("cookie-parser");
-app.use(cookies());
-
 // Entornos
 const carpeta = path.basename(path.join(__dirname));
 const entProd = carpeta == "1-Actual";
@@ -22,6 +18,4 @@ app.listen(puerto, () => console.log("\nELC Redirecciona - Servidor funcionando.
 // Redirige a 'peliculas.elc'
 const inicio = "https://peliculas";
 const urlHost = inicio + (entProd ? ".elc.lat" : entPrueba ? "2.elc.lat" : ".elc:3001");
-app.use((req, res) => {
-	return res.redirect(urlHost + req.originalUrl);
-});
+app.use((req, res) => res.redirect(urlHost + req.originalUrl));
